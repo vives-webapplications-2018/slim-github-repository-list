0a177b51b7e90a7739e91a71c45efeb2ae558c04
Sille Van Landschoot <info@sillevl.be>
2018-10-16 10:08:18 +0200

    add dotenv load as middleware to project

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,10 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$app->add( function($request, $response, $next){
+    $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+    $dotenv->load();
+
+    return $next($request, $response);
+});
