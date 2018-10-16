cd6157e0ddd1daeafa45f1cf73f08ce749d6046d
Sille Van Landschoot <info@sillevl.be>
2018-10-16 10:09:20 +0200

    make use of library in routes file

--- src/routes.php
+++ src/routes.php
@@ -5,9 +5,11 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
+
+    $args['content'] = new \sillevl\App\App(getenv('GITHUB_TOKEN'));
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
